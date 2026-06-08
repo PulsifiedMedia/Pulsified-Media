68b4bacc4315b1e5d74e35c5850d3dc197fef47b
Dalton Kollar <daltonkollar@Bookie.local>
2026-06-07 22:40:05 -0400

    Connect site to Supabase

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// JavaScript Document
+const SUPABASE_URL = "https://spemazulkhfyvjqoxqpf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_6oMtXN5wRLHMo7ysjh-jsQ_GNsYOLlw";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function loadReviews() {
+  const { data, error } = await supabaseClient
+    .from("reviews")
+    .select("*")
+    .order("published_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log("Reviews:", data);
+}
+
+loadReviews();
